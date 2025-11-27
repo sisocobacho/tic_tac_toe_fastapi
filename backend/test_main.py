3c940dcd35d531f675b49d947638dd114e029525
Osvaldo Cobacho <oobacho@countigo.cu>
2025-11-27 12:37:51 -0500

    test(api): Add test for health endpoint

--- backend/test_main.py
+++ backend/test_main.py
@@ -9,5 +9,14 @@ def test_get_root_endpoint():
     response = client.get("/")
     assert response.status_code == 200
 
+def test_get_healt_endpoint():
+    """Test api root"""
+    response = client.get("/health")
+    assert response.status_code == 200
+    status = response.json()["status"]
+    timestamp = response.json()["timestamp"]
+    assert status == "healthy"
+    assert timestamp
+    
 if __name__ == "__main__":
     pytest.main([__file__, "-v"])
