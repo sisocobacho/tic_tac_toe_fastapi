f250dd7d6874ea636d91ae8a1de630c19999a422
Osvaldo Cobacho <oobacho@countigo.cu>
2025-12-03 16:30:42 -0500

    refactor: Improve imports in init for schema and models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,2 +1,4 @@
 from .game import GameModel
 from .user import User
+
+__all__ = ["User", "GameModel"]
--- backend/app/schema/__init__.py
+++ backend/app/schema/__init__.py
@@ -1,1 +1,3 @@
 from .user import UserCreate, UserLogin, UserResponse, Token
+
+__all__ = ["UserCreate", "UserLogin", "UserResponse", "Token"]
